9a28558de73f7004cbfa06d5d40bb5dd162cf830
JoaoPedroBackXavier <joaopedro.backxavier@gmail.com>
2023-05-03 16:20:07 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,45 @@
+//Exercicio de escrita de codigo
+
+//1
+//const nome = prompt ("Digite seu nome")
+//const email = prompt ("Digite seu E-mail")
+//console.log ("O E-Mail "+email+" foi cadastrado com sucesso. Seja bem vindo(a)  "+nome+"")
+
+//2
+
+//const comidafav2 = prompt ("Digite sua comida favorita")
+//comidasfav[1] = comidafav2
+//const comidasfav =  ["Pizza","Fritas","Frango a milanesa","Palha italiana","Camarão"]
+//console.log ("Estas são minhas comidas favoritas ")
+//console.log (comidasfav[0])
+//console.log (comidasfav[1])
+//console.log (comidasfav[2])
+//console.log (comidasfav[3])
+//console.log (comidasfav[4])
+
+//3
+
+//const ListaDeTarefas = []
+//const Tarefas = prompt ("Digite uma tarefa sua diaria")
+//const Tarefas2 = prompt ("Digite outra de suas tarefas diarias")
+//const Tarefas3 = prompt ("Digite mais uma de suas tarefas diarias")
+//ListaDeTarefas.push(Tarefas,Tarefas2,Tarefas3)
+//console.log (ListaDeTarefas)
+//const indice = prompt ("Digite um numero de 0 a 2 para remover uma de suas tarefas")
+//ListaDeTarefas.splice(indice,1)
+//console.log (ListaDeTarefas)
+
+//Desafio
+
+//1
+
+//let frase = prompt ("Digite sua frase favorita")
+//let frasearray = frase.split(" ");
+//console.log (frasearray);
+
+//2
+
+//const array1 = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"];
+//const achouIndex = array1.findIndex(indice => indice ==="Abacaxi")
+//const tamanho = array1.length
+//console.log (achouIndex,tamanho)
